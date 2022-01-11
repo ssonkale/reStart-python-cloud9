7196e1c154cebb5f1849bd901791371ecd5ce025
ssonkale <64415248+ssonkale@users.noreply.github.com>
2022-01-11 11:27:38 +0530

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,6 @@
+name="Priya"
+color="Red"
+animal="Dog"
+
+print("{}, you like a {} {}!".format(name,color,animal))
+print("{} + {} ={}".format(1,2,3))
---
+++ prog3.py
@@ -0,0 +1,31 @@
+L1= [1,2,3]
+
+print(id(L1))
+
+L1.append(10)
+L1.insert(5,2) 
+
+
+
+print(L1)
+print(id(L1))
+
+
+#print(L1)
+
+
+'''= [6,7,8,9]
+
+print(id(L1))
+print(id(L2))'''
+
+
+
+
+
+
+
+
+
+
+
